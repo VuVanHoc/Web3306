9350c9fec1c8882fd91e55b3a04952a80cf8a2a8
VuVanHoc <hocvanvu1999@gmail.com>
2019-11-05 11:52:02 +0700

    remove authenticate

--- src/main/java/com/uet/k62/web/system/examination/controller/StudentController.java
+++ src/main/java/com/uet/k62/web/system/examination/controller/StudentController.java
@@ -2,13 +2,14 @@ package com.uet.k62.web.system.examination.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
 	
 	@GetMapping(value = {"/", "/login", "/logout"})
 	public String index() {
-		return "index";
+		return "Signin";
 	}
 	
 	@GetMapping(value = "/student/dashboard")
@@ -16,5 +17,9 @@ public class StudentController {
 		System.out.println("STUDENT DASHBOARD");
 		
 		return "exam";
+	}
+	@GetMapping(value = "/authenticate")
+	public String authenticate(){
+		return "authenticate";
 	}
 }

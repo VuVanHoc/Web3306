96dc031a818670e3900a71ce75d0a50d79fc28f8
VuVanHoc <hocvanvu1999@gmail.com>
2019-11-19 16:43:22 +0700

    question

--- src/main/java/com/uet/k62/web/system/examination/restcontroller/AnswerController.java
+++ src/main/java/com/uet/k62/web/system/examination/restcontroller/AnswerController.java
@@ -2,12 +2,15 @@ package com.uet.k62.web.system.examination.restcontroller;
 
 import com.uet.k62.web.system.examination.model.RestBody;
 import com.uet.k62.web.system.examination.model.dtos.AnswerRequestDTO;
+import com.uet.k62.web.system.examination.model.entity.Answer;
 import com.uet.k62.web.system.examination.service.AnswerService;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/answers")

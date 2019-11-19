1a3bb07660141c014c9b0d92700c9e870f358db8
VuVanHoc <hocvanvu1999@gmail.com>
2019-11-19 13:01:01 +0700

    show questions

--- src/main/java/com/uet/k62/web/system/examination/service/impl/QuestionServiceImpl.java
+++ src/main/java/com/uet/k62/web/system/examination/service/impl/QuestionServiceImpl.java
@@ -2,14 +2,16 @@ package com.uet.k62.web.system.examination.service.impl;
 
 import com.uet.k62.web.system.examination.error.QuestionNotFoundException;
 import com.uet.k62.web.system.examination.model.RestBody;
-import com.uet.k62.web.system.examination.model.dtos.*;
+import com.uet.k62.web.system.examination.model.dtos.CorrectAnswerResponseDTO;
+import com.uet.k62.web.system.examination.model.dtos.QuestionAnswerResponseDTO;
+import com.uet.k62.web.system.examination.model.dtos.QuestionRequestDTO;
+import com.uet.k62.web.system.examination.model.dtos.QuestionResponseDTO;
 import com.uet.k62.web.system.examination.model.entity.Answer;
 import com.uet.k62.web.system.examination.model.entity.Question;
 import com.uet.k62.web.system.examination.repository.AnswerRepository;
 import com.uet.k62.web.system.examination.repository.QuestionRepository;
 import com.uet.k62.web.system.examination.repository.QuestionTypeRepository;
 import com.uet.k62.web.system.examination.service.QuestionService;
-import com.uet.k62.web.system.examination.service.QuestionTypeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
@@ -18,7 +20,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.math.BigInteger;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -81,36 +82,37 @@ public class QuestionServiceImpl implements QuestionService {
 
     @Override
     public RestBody getAllQuestions(Integer pageNo, Integer pageSize) {
-        Pageable paging = PageRequest.of(pageNo, pageSize);
-        List<QuestionResponseDTO> questionResponseListDTO = new ArrayList<>();
-        Page<Question> questions = questionRepository.findAllByDeletedIsFalse(paging);
-	    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm a");
-        if (questions.hasContent()) {
-        	int index = pageNo * pageSize + 1;
-            for (Question question : questions.getContent()){
-                QuestionResponseDTO single = new QuestionResponseDTO();
-                BeanUtils.copyProperties(question, single);
-                
-                single.setQuestionTypeCode(this.getQuestionTypeCode(question.getQuestionTypeId()));
-	            if(single.getQuestionTypeCode().equals("MC")){
-		            single.setQuestionTypeCode("Multiple Choice");
-	            } else if(single.getQuestionTypeCode().equals("SA")){
-		            single.setQuestionTypeCode("Fill in blank");
-	            } else if(single.getQuestionTypeCode().equals("TF")){
-		            single.setQuestionTypeCode("Yes/No or True/False");
-	            }
-	            single.setIndex(index);
-	            index++;
-                single.setCreatedDate(simpleDateFormat.format(question.getCreatedDate()));
-                questionResponseListDTO.add(single);
-            }
-        } else {
-            return RestBody.success("Không có câu hỏi nào");
-        }
-
-        return RestBody.success(questionResponseListDTO);
+        List<QuestionResponseDTO> questionResponseDTOS = getAllQuestion(pageNo, pageSize);
+        return RestBody.success(questionResponseDTOS);
     }
 
+    public List<QuestionResponseDTO> getAllQuestion(int pageNum, int pageSize) {
+	    Pageable paging = PageRequest.of(pageNum, pageSize);
+	    List<QuestionResponseDTO> questionResponseListDTO = new ArrayList<>();
+	    Page<Question> questions = questionRepository.findAllByDeletedIsFalse(paging);
+	    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm a");
+	    if (questions.hasContent()) {
+		    int index = pageNum * pageSize + 1;
+		    for (Question question : questions.getContent()) {
+			    QuestionResponseDTO single = new QuestionResponseDTO();
+			    BeanUtils.copyProperties(question, single);
+			
+			    single.setQuestionTypeCode(this.getQuestionTypeCode(question.getQuestionTypeId()));
+			    if (single.getQuestionTypeCode().equals("MC")) {
+				    single.setQuestionTypeCode("Multiple Choice");
+			    } else if (single.getQuestionTypeCode().equals("SA")) {
+				    single.setQuestionTypeCode("Fill in blank");
+			    } else if (single.getQuestionTypeCode().equals("TF")) {
+				    single.setQuestionTypeCode("Yes/No or True/False");
+			    }
+			    single.setIndex(index);
+			    index++;
+			    single.setCreatedDate(simpleDateFormat.format(question.getCreatedDate()));
+			    questionResponseListDTO.add(single);
+		    }
+	    }
+	    return questionResponseListDTO;
+    }
     @Override
     public RestBody getQuestion(Integer id) {
         Question question = questionRepository.findOneByIdAndDeletedIsFalse(id);
@@ -143,7 +145,7 @@ public class QuestionServiceImpl implements QuestionService {
                 arrayList.add(i);
             }
         }
-        int correctIndex[] = new int[arrayList.size()];
+	    int[] correctIndex = new int[arrayList.size()];
         for (int i = 0; i < arrayList.size(); i++) {
             correctIndex[i] = arrayList.get(i);
         }

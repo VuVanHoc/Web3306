270ddca154459a681bcb6e3062213106a89ca79e
VuVanHoc <hocvanvu1999@gmail.com>
2019-11-26 02:25:29 +0700

    request data create new question from client to server

--- src/main/java/com/uet/k62/web/system/examination/model/dtos/CreateQuestionAndAnswerRequest.java
+++ src/main/java/com/uet/k62/web/system/examination/model/dtos/CreateQuestionAndAnswerRequest.java
@@ -2,11 +2,15 @@ package com.uet.k62.web.system.examination.model.dtos;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class CreateQuestionAndAnswerRequest {
 	
+	private String questionType;
 	private String question;
 	private String image;
+	private List<CreateAnswerDTO> answerDTOList;
 	
 	
 }
